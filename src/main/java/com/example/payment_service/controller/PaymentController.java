e177afc539478c0d344160b954da2149d20f1724
Arthur <poncin.arthur@proton.me>
2025-12-01 19:33:51 +0100

    Init

---
+++ src/main/java/com/example/payment_service/controller/PaymentController.java
@@ -0,0 +1,44 @@
+package com.example.payment_service.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.payment_service.model.Payment;
+import com.example.payment_service.service.PaymentService;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final PaymentService service;
+
+    public PaymentController(PaymentService service) {
+        this.service = service;
+    }
+
+    // Créer un paiement
+    @PostMapping("/{registrationId}")
+    public Payment createPayment(@PathVariable Long registrationId) {
+        return service.createPayment(registrationId);
+    }
+
+    // Récupérer un paiement
+    @GetMapping("/{id}")
+    public Payment getPayment(@PathVariable Long id) {
+        return service.getPayment(id);
+    }
+
+    // Récupérer l'historique d'une inscription
+    @GetMapping("/registration/{regId}")
+    public List<Payment> getByRegistration(@PathVariable Long regId) {
+        return service.getPaymentsByRegistration(regId);
+    }
+
+    // Valide le paiement
+    @PutMapping("/{id}/validate")
+    public Payment validatePayment(@PathVariable Long id) {
+        return service.validatePayment(id);
+    }
+
+}
---
+++ src/main/java/com/example/payment_service/service/PaymentService.java
@@ -0,0 +1,43 @@
+package com.example.payment_service.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.example.payment_service.model.Payment;
+import com.example.payment_service.repository.PaymentRepository;
+
+@Service
+public class PaymentService {
+
+    // Constructeur
+    private final PaymentRepository repository;
+
+    public PaymentService(PaymentRepository repository) {
+        this.repository = repository;
+    }
+
+    // Créer un paiement
+    public Payment createPayment(Long registrationId) {
+        Payment payment = new Payment(registrationId);
+        return repository.save(payment);
+    }
+
+    // Récupérer un paiement
+    public Payment getPayment(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Paiement introuvable"));
+    }
+
+    // Récupère tout les paiements d'une inscription
+    public List<Payment> getPaymentsByRegistration(Long registrationId) {
+        return repository.findByRegistrationId(registrationId);
+    }
+
+    // Met à jour le statut
+    public Payment validatePayment(Long id) {
+        Payment payment = getPayment(id);
+        payment.setStatut("SUCCESS");
+        payment.setReference("BANK-" + System.currentTimeMillis());
+        return repository.save(payment);
+    }
+}
